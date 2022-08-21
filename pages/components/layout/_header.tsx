7d1ceba7d0f57cfe7cf12e0838748f0a1d46c1ca
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2022-08-21 00:23:29 -0400

    new-story button in header

--- pages/components/layout/_header.tsx
+++ pages/components/layout/_header.tsx
@@ -4,13 +4,12 @@ export function Header() {
     <header>
       <div className="logo">
         <Link href={'/'}>
-          <a>
-            <img src="officelogo.jpg" alt="navigate to home page" height={60} width={90} />
-          </a> 
+          <a><img src="officelogo.jpg" alt="navigate to home page" height={60} width={90} /></a> 
         </Link>
       </div>
       <nav>
         <ul>
+          <li><Link href={'/new-story'}><a><div className='add-story-button'>+</div></a></Link></li>
           <li><Link href={'/signup'}>Sign Up</Link></li>
         </ul>
       </nav>
---
+++ pages/new-story/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const NewStory: NextPage = () => {
+  return(
+    <h1>newstory</h1>
+  )
+}
+
+export default NewStory
