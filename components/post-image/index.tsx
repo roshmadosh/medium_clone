42a7b2b19df174904a3e708daab6a19d9d0a4f1f
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2022-09-06 19:30:02 -0400

    post-image component refactor

--- components/post-image/index.tsx
+++ components/post-image/index.tsx
@@ -4,7 +4,10 @@ import { useState } from 'react';
 export const PostImage = () => {
   const [imageSrc, setImageSrc] = useState('');
   const onChosenHandler = async (e) => {
-    const file: File = e.target.files.item(0);
+    const file: File | undefined = e.target.files?.item(0);
+    if (!file) { 
+      return; 
+    }
     if (file.size > 1e7) {
       alert('Your image cannot be greater than 10MB in size.');
       e.target.value = null;
@@ -14,14 +17,24 @@ export const PostImage = () => {
 
   return (
     <div id="post-image-container">
-      <input type="file" name="post-image-file" id="post-image-file" accept="image/*" onChange={onChosenHandler}/>
+      <div id="add-post-image">
+        <label htmlFor="post-image-file">+</label>
+        <input type="file" 
+          name="post-image-file" 
+          id="post-image-file" 
+          accept="image/*" 
+          onChange={onChosenHandler}
+        />
+      </div>
+
       <div id="post-image">
+        {!imageSrc && <img src="photo-icon.svg" id="photo-placeholder-icon" alt="photo-icon" />}
         {imageSrc && 
           <Image 
             src={imageSrc} 
             layout="fill" 
             objectFit="contain" 
-            alt="post-image"
+            alt="user provided post image"
           />}
       </div>
     </div>
